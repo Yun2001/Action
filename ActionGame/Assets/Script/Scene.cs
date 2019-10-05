80404505fc0a46e943f83111efd00e2d7e19e0a7
nieda <ayaki.nieda@gmail.com>
2019-10-06 00:52:26 +0900

    sceneScript追加(仮)

---
+++ ActionGame/Assets/Script/Scene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Scene : MonoBehaviour {
+
+    public enum sceneName
+    {
+        TItle,
+        StageSelect,
+        GameMain,
+        Result
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //制作停止
+    //void SceneChange()
+    //{
+    //    switch (sceneName)
+    //    {
+    //        case TItle:
+    //            SceneManager.LoadSceneAsync("StageSelect", LoadSceneMode.Single);
+    //            break;
+    //        case 1:
+    //            SceneManager.LoadSceneAsync("StageSelect", LoadSceneMode.Single);
+    //            break;
+    //        case 2:
+    //            SceneManager.LoadSceneAsync("StageSelect", LoadSceneMode.Single);
+    //            break;
+    //    }          
+    //}
+
+
+
+}
